829c2e91bf32cd3d5b8e72d8e4f608717ebccfc1
xHanax <hana8951@naver.com>
2021-11-15 23:13:45 +0900

    Start Project

---
+++ js/main.js
@@ -0,0 +1,164 @@
+//검색창
+const searchEl = document.querySelector('.search');//문서에서 .search를 찾음
+const searchInputEl = searchEl.querySelector('input');//.search에서 input을 찾음
+
+searchEl.addEventListener('click', function(){
+  searchInputEl.focus();
+}); //searchEl을 클릭하면 searchInputEl에도 포커스를 적용해라
+
+searchInputEl.addEventListener('focus', function(){
+  searchEl.classList.add('focused');//searchInputEl에 포커스가 되면 searchEl에 focused 클래스를 추가해라
+  searchInputEl.setAttribute('placeholder', '통합검색');//searchInputEl에 html의 속성을 부여함, setAttribute('속성이름','속성값')
+});
+
+searchInputEl.addEventListener('blur', function(){
+  searchEl.classList.remove('focused');//searchInputEl에 포커스가 해제(블러)되면 searchEl에 focused 클래스를 삭제해라
+  searchInputEl.setAttribute('placeholder', '');//searchInputEl에 html의 속성을 부여함
+});
+
+
+//뱃지
+const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
+
+window.addEventListener('scroll', _.throttle(function(){
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    //배지 숨기기
+    //gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+    // 상단이동버튼 보이기
+    gsap.to('#to-top', .2, {
+      x: 0
+    });
+  } else {
+    //배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+    // 상단이동버튼 숨기기
+    gsap.to(toTopEl, .2, {
+      x: 100
+    });
+  }
+}, 300)); //스크롤 할때마다 함수 실행하는데 과부하 되는걸 막기위해서 throttle이라는걸 사용함
+// -.throttle(함수,시간);
+
+// 상단이동버튼 클릭
+toTopEl.addEventListener('click', function(){
+  gsap.to(window, .7, {
+    scrollTo: 0
+  });
+});
+
+//비주얼 페이드인
+const fadeEls = document.querySelectorAll('.visual .fade-in'); //.visual의 .fade-in을 모두 찾아서 fadeEls에 할당
+fadeEls.forEach(function(fadeEl, index){
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1
+  });
+});
+
+
+//공지사항 스와이프
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+});
+
+
+//프로모션 스와이프
+new Swiper('.promotion .swiper-container', {
+  slidesPerView: 3, //한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, //슬라이드 사이 여백
+  centeredSlides: true,  //1번 슬라이드가 가운데에 보임
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination',  //페이지 번호 요소 선택자
+    clickable: true //사용자의 페이지 번호 요소 제어
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+//하단 스와이프
+new Swiper('.awards .swiper-container', {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: '.awards .swiper-prev',
+    nextEl: '.awards .swiper-next',
+  }
+});
+
+//프로모션 버튼 토글
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function(){
+  isHidePromotion = !isHidePromotion
+  if (isHidePromotion) {
+    //숨김처리
+    promotionEl.classList.add('hide');
+  } else {
+    //보임처리
+    promotionEl.classList.remove('hide');
+  }
+});
+
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+//유튜브 영상 위 애니메이션 동작
+function floatingObject(selector, delay, size){
+  // gsap.to(요소,시간,옵션);
+  gsap.to(
+    selector, // 선택자
+    random(1.5, 2.5), // 애니메이션 동작 시간
+    { // 옵션
+      y: size,
+      repeat: -1,
+      yoyo: true,  // 한번 재생된 애니메이션을 거꾸로 다시 실행
+      ease: Power1.easeInOut,
+      delay: random(0, delay)
+    }
+  );
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
+
+
+// 매직 스크롤
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function(spyEl){
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl,  // 보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
+});
+
+
+// 연도계산
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();  // 2021
